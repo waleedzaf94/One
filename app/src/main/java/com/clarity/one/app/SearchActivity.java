a588b6c108f095ccce820ac8359553c2f67fe96d
Waleed Zafar <waleedzaf94@gmail.com>
2015-07-08 13:32:47 +0800

    Added Search Page Layout

---
+++ app/src/main/java/com/clarity/one/app/SearchActivity.java
@@ -0,0 +1,19 @@
+package com.clarity.one.app;
+
+import android.app.Activity;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.clarity.one.R;
+
+public class SearchActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+
+}
